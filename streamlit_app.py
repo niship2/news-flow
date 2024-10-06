4aaf604b1d5f0c982981494b313b64b81c874527
niship2 <48500095+niship2@users.noreply.github.com>
2024-10-06 08:55:16 +0000

    modify:期間指定可能に

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,26 +5,28 @@ import streamlit_mermaid as stmd
 
 from agents.agent1 import agent_builder
 
+
 st.set_page_config(layout="wide")
 
 st.title("ニュース検索")
-st.write("過去２週間の情報を中心に")
 
 searchword = st.text_input("キーワード入力",value="sakana AI")
 #MODEL_NAME = st.sidebar.selectbox("モデル選択",["gpt-4o","gemini"])
-SELECTED_TOOLS = st.sidebar.multiselect("ツール選択",["search_google_news","search_google_news_JA","search_bing_news","search_wikipedia","search_web","search_youcom"],default=["search_google_news","search_google_news_JA"])
+SELECTED_TOOLS = st.sidebar.multiselect("ツール選択",["search_google_news","search_google_news_JA","search_bing_news","search_wikipedia","search_tavily","search_youcom"],default=["search_google_news","search_google_news_JA"])
+TIME_OP= st.sidebar.selectbox("期間選択",["直近24時間","直近1週間","過去2週間","直近1ヶ月","過去1年"],index=2)
+
 
 if st.button("検索"):
     st.sidebar.warning("抽出に時間がかかる場合があるので注意。今後改善していきます")
     graph= agent_builder(SELECTED_TOOLS)
 
-    with st.sidebar.expander("抽出フロー"):
+    #with st.sidebar.expander("抽出フロー"):
         #st.write(graph.get_graph().print_ascii())
-        stmd.st_mermaid(graph.get_graph().draw_mermaid())
+        #stmd.st_mermaid(graph.get_graph().draw_mermaid())
         #st.markdown(graph.get_graph().draw_mermaid())
 
 
-    result = graph.invoke({"question": searchword})
+    result = graph.invoke({"question": searchword,"time_op":TIME_OP})
 
     articles = [[x.title,x.date.strftime("%y-%m-%d"),x.source_url,x.japanese_translation] for x in  result['answer'].content]
 
